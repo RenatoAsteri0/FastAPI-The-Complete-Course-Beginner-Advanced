95a871d53284cac91c2fce1f99e0204c3b2879bd
renato <renato.asterio@hotmail.com>
2026-05-02 14:07:14 -0300

    add

--- FastAPI-The-Complete-Course-main/Project 2/book.py
+++ FastAPI-The-Complete-Course-main/Project 2/book.py
@@ -21,15 +21,26 @@ class Book:
 
 
 class Book_Request(BaseModel):
-    id: Optional[int] = Field("Campo não é necessário", default=None)
+    id: Optional[int] = Field(description="Campo não é necessário", default=None)
     titulo: str = Field(min_length=3, max_length=20)
     autor: str
     descricao: str
     avaliacao: int = Field(ge=0, le=10)
 
+    model_config = {
+        "json_schema_extra": {
+
+            "example": {
+                "titulo": "titulo um",
+                "autor": "renato",
+                "descricao": "um livro bem legal",
+                "avaliacao": 8
+            }
+        }
+    }
 
 BOOKS = [
-    Book(1, 'titulo um', 'renato', 'um livro bem legal', 8),
+    Book(1, 'titulo um', 'renato', 'um livro bem legal', 10),
     Book(2, 'titulo dois', 'leticia', 'um livro muito legal', 9),
     Book(3, 'titulo tres', 'renato', 'um livro excelente', 10),
     Book(4, 'titulo quatro', 'joao', 'um livro ruim', 3)
@@ -38,6 +49,20 @@ BOOKS = [
 @app.get("/books")
 async def listar_livros():
     return BOOKS
+
+@app.get("/books/{book_id}")
+async def pegar_livro_id(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+@app.get("/books/")
+async def pegar_livro_avaliacao(book_avaliacao: int):
+    livros_filtrados: list[int] = []
+    for book in BOOKS:
+        if book.avaliacao == book_avaliacao:
+            livros_filtrados.append(book)
+    return livros_filtrados
 
 @app.post("/create-book")
 async def criar_livro(book_request: Book_Request):
@@ -48,3 +73,12 @@ async def criar_livro(book_request: Book_Request):
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+@app.put("/books/atualizar_livro")
+async def atualizar_livro(book: Book_Request):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            return {"mensagem": "Livro atualizado com sucesso"}
+
+    return {"mensagem": "Livro não encontrado"}
